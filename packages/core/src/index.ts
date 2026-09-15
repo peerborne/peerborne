030ac77ff2a4c96bb6bb7e0417b686e6abd39759
Robert Chu <allegrormc@gmail.com>
2026-09-15 01:41:56 -0700

    fix(core): reject backing ACL reentry

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -32,7 +32,11 @@ import {
 import { SubtleCrypto } from './auth-subtlecrypto.js';
 import { ACLProvider } from './acl-provider.js';
 import { KeychainProvider } from './keychain-provider.js';
-import { ACL } from './acl.js';
+import {
+  ACL,
+  ACLOperationInProgressError,
+  retryACLConflict,
+} from './acl.js';
 import {
   Keychain,
   TransactionalKeychain,
@@ -185,6 +189,8 @@ export * from './webcrypto-group-state-protector.js';
 
 export {
   ACL,
+  ACLOperationInProgressError,
+  retryACLConflict,
   ACLProvider,
   SubtleCrypto,
   Peerborne,
